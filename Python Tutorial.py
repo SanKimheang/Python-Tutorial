7f069935adbe60862fbd7eb4bf6a497612e73cfa
kimheang <kimheangtkd@gmail.com>
2025-05-09 00:31:33 +0700

    add a new lesson to the tutorial

--- Python Tutorial.py
+++ Python Tutorial.py
@@ -109,3 +109,66 @@ and can only be used inside that function. The global variable with the same nam
 #     x = "beautiful"
 # myfunction()
 # print("Python is " + x)
+
+# Built-in Data Types
+"""
+ Text Type:	str
+ Numeric Types:    int, float, complex
+ Sequence Types:	list, tuple, range
+ Mapping Type:	dict
+ Set Types:	set, frozenset
+ Boolean Type:	bool
+ Binary Types:	bytes, bytearray, memoryview
+ None Type:	NoneType
+"""
+# x = "Hello World" # str
+# x = 20 # int
+# x = 20.5 # float
+# x = 1j # complex
+# x = ["apple", "banana", "cherry"] # list
+# x = ("apple", "banana", "cherry") # tuple
+# x = range(6) # range
+# x = {"name": "John", "age": 36} # dict
+# x = {"apple", "banana", "cherry"} # set
+# x = frozenset({"apple", "banana", "cherry"}) # frozenset
+# x = True # bool
+# x = b"Hello" # bytes
+# x = bytearray(5) # bytearray
+# x = memoryview(bytes(5)) # memoryview
+# x = None # NoneType
+
+# Python Numbers
+# x = 1    # int
+# y = 2.8  # float
+# z = 1j   # complex
+
+# Intergers
+# x = 1
+# y = 35656222554887711
+# z = -3255522
+
+# Float
+# x = 1.10
+# y = 1.0
+# z = -35.59
+
+# Float can also be scientific numbers with an "e" to indicate the power of 10.
+# x = 35e3 # 35 * 10^3
+# y = 12E4 # 12 * 10^4
+# z = -87.7e100 # -87.7 * 10^100
+
+# Complex Numbers
+# x = 3 + 5j
+# y = 5j
+# z = -5j
+
+# Type Conversion
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+#convert from int to float:
+a = float(x)
+#convert from float to int:
+b = int(y)
+#convert from int to complex:
+c = complex(x)
