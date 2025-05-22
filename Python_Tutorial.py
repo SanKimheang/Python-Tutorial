bf16492cf1b0722438af442888e898a4c4d6d621
kimheang <kimheangtkd@gmail.com>
2025-05-22 17:42:00 +0700

    finishing If Else Condition

--- Python_Tutorial.py
+++ Python_Tutorial.py
@@ -1184,3 +1184,34 @@ a = 330
 b = 330
 print("A") if a > b else print("=") if a ==b else print("B")
 # And
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+    print("Both conditions are True")
+# Or 
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+    print("At lest one of the conditions is True")
+# Not
+# The not keyword is a logical operator that is used to reverse the result of the condition.
+a = 33
+b = 200
+if not a > b:
+    print("a is Not greater than b")
+# Nested If
+x = 41
+if x > 10:
+    print("Above ten,")
+    if x > 20:
+        print("and also above 20!")
+    else:
+        print("but not above 20.")
+# The pass Statement
+# The pass statement in Python is a null operation — when it is executed, nothing happens. It
+a = 33
+b = 200
+if b > a:
+    pass
