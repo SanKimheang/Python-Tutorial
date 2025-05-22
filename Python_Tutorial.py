8042c7ddda09bb83ad9cad0f6fe404aa15ce5e56
kimheang <kimheangtkd@gmail.com>
2025-05-22 17:40:12 +0700

    finishing If Else Condition

--- Python_Tutorial.py
+++ Python_Tutorial.py
@@ -1137,3 +1137,50 @@ for x, obj in myfriend.items():
         print(y, ":", obj[y])
 
 # Python if ... else
+"""
+Equals: a == b
+Not Equals: a != b
+Less than: a < b
+Less than or equal to: a <= b
+Greater than: a > b
+Greater than or equal to: a >= b
+"""
+# If statement
+a = 33
+b = 200
+if b > a:
+    print("b is greater than a")
+# Elif
+a = 33
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a==b:
+    print("a and b are equal")
+# Else
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+# or
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+# Short Hand If
+if a > b: print("a is greater than b")
+# Short Hand If ... Else
+a = 2
+b = 330
+print("A") if a > b else print("B")
+# One Line If else statement, with 3 conditions
+a = 330
+b = 330
+print("A") if a > b else print("=") if a ==b else print("B")
+# And
